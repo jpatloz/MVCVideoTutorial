2a4c68fa8e798eb3b39d769c8f4290664de4a77d
DESKTOP-3I3N22C\JesusPatricio <csi22-jpatano@colegioaltair.net>
2022-11-14 11:40:32 +0100

    Agregar archivos de proyecto.

---
+++ MVCVideoTutorial/Controllers/UsuariosController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCVideoTutorial.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        // GET: Usuarios
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ListaUsuarios()
+        {
+            List<string> listaUsuarios = new List<string>();
+            listaUsuarios.Add("Jesus");
+            listaUsuarios.Add("Pablo");
+            listaUsuarios.Add("Mena");
+            listaUsuarios.Add("Juanca");
+            listaUsuarios.Add("Javi");
+            return View(listaUsuarios);
+        }
+    }
+}
---
+++ MVCVideoTutorial/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCVideoTutorial.MvcApplication" Language="C#" %>
