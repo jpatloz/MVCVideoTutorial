652e59ff7ae6fc37b37cf7a5c926f55b00b2e793
DESKTOP-3I3N22C\JesusPatricio <csi22-jpatano@colegioaltair.net>
2022-11-16 13:51:26 +0100

    Parte 3 del vídeo tutorial

--- MVCVideoTutorial/Controllers/UsuariosController.cs
+++ MVCVideoTutorial/Controllers/UsuariosController.cs
@@ -22,7 +22,26 @@ namespace MVCVideoTutorial.Controllers
             listaUsuarios.Add("Mena");
             listaUsuarios.Add("Juanca");
             listaUsuarios.Add("Javi");
+
+            //ViewBag.ListaUsuarios = listaUsuarios; //No puede pasar información de un controlador a otro, solo del controlador a las vistas
+            //TempData["ListaUsuarios"] = listaUsuarios; //Solo puede mandar la información una vez
+            //Session["ListaUsuarios"] = listaUsuarios; //Envía la información entre ontroladores hasta que caduque la sesión
+            ViewData["ListaUsuarios"] = listaUsuarios; //Es muy similar al ViewBag pero necesita un casting
+
+
             return View(listaUsuarios);
         }
+
+        [HttpPost]
+        public ActionResult ListaUsuarios(string selUsuarios)
+        {
+            ViewBag.Nombre = selUsuarios;
+            //List<string> listaUsuarios = ViewBag.ListaUsuarios;
+            //List<string> listaUsuarios = (List<string>)TempData["ListaUsuarios"];//Es necesario hacer un casting en posibles correciones
+            //List<string> listaUsuarios = (List<string>)Session["ListaUsuarios"];
+            List<string> listaUsuarios = (List<string>)ViewData["ListaUsuarios"];
+            return View(listaUsuarios);
+        }
+
     }
 }
